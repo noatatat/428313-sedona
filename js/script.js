9320070dd08152f90255ae83da8a6ef52e4507d0
noatatat <t.b.obuhov@gmail.com>
2017-09-06 17:00:14 +0300

    Движенние вверх

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
 var formButton  = document.querySelector(".order-form-button");
 var orderForm = document.querySelector(".order");
-
-
 var arrival = orderForm.querySelector("[name=arrival]");
 var departure = orderForm.querySelector("[name=departure]");
 var adult = orderForm.querySelector("[name=adult]");
 var children = orderForm.querySelector("[name=children]");
-
 var adultNumberLess = document.querySelector(".adult-count .number-less");
 var adultNumberMore = document.querySelector(".adult-count .number-more");
 var childrenNumberLess = document.querySelector(".children-count .number-less");
 var childrenNumberMore = document.querySelector(".children-count .number-more");
 
+/*var orderOpen = document.querySelector(".order-open");
+var orderHidden = document.querySelector(".order-hidden");*/
+
 formButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   orderForm.classList.toggle("order-hidden");
+  /*console.log(orderHidden);
+  console.log(orderOpen);
+  if ((orderOpen == null) && (orderHidden == null)) {
+    orderForm.classList.add("order-hidden");
+  }
+
+  if ((orderOpen == false) && (orderHidden == true)){
+    orderForm.classList.remove("order-hidden");
+    orderForm.classList.add("order-open");
+  }
+  if ((orderOpen == true) && (orderHidden == false)){
+    orderForm.classList.remove("order-open");
+    orderForm.classList.add("order-hidden");
+  }*/
 });
 
 orderForm.addEventListener("submit", function (evt) {
