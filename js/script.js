0dfd71778f47cda836c17ab2f42b38a8771ac825
noatatat <t.b.obuhov@gmail.com>
2017-09-07 08:16:51 +0300

    Доработана анимация

--- js/script.js
+++ js/script.js
@@ -8,40 +8,25 @@ var adultNumberLess = document.querySelector(".adult-count .number-less");
 var adultNumberMore = document.querySelector(".adult-count .number-more");
 var childrenNumberLess = document.querySelector(".children-count .number-less");
 var childrenNumberMore = document.querySelector(".children-count .number-more");
+var orderOpen = orderForm.classList.contains("order-open");
+var orderHidden = orderForm.classList.contains("order-hidden");
+var storageAdult = localStorage.getItem("adult");
+var storageChildren = localStorage.getItem("children");
 
-/*var orderOpen = document.querySelector(".order-open");
-var orderHidden = document.querySelector(".order-hidden");*/
 
 formButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  orderForm.classList.toggle("order-hidden");
-  /*console.log(orderHidden);
-  console.log(orderOpen);
-  if ((orderOpen == null) && (orderHidden == null)) {
-    orderForm.classList.add("order-hidden");
-  }
 
-  if ((orderOpen == false) && (orderHidden == true)){
-    orderForm.classList.remove("order-hidden");
-    orderForm.classList.add("order-open");
-  }
-  if ((orderOpen == true) && (orderHidden == false)){
-    orderForm.classList.remove("order-open");
+  if (!orderOpen && !orderHidden) {
     orderForm.classList.add("order-hidden");
-  }*/
-});
-
-orderForm.addEventListener("submit", function (evt) {
-  if (!arrival.value || !departure.value || !adult.value || !children.value) {
-    evt.preventDefault();
-    orderForm.classList.remove("order-error");
-    orderForm.offsetWidth = orderForm.offsetWidth;
-    orderForm.classList.add("order-error");
+    orderHidden = orderForm.classList.contains("order-hidden");;
+  } else {
+    orderForm.classList.toggle("order-open");
+    orderOpen = orderForm.classList.contains("order-open");
+    orderForm.classList.toggle("order-hidden");
+    orderHidden = orderForm.classList.contains("order-hidden");
   }
 });
-
-adult.value = 2;
-children.value = 0;
 
 adultNumberLess.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -59,7 +44,6 @@ adultNumberMore.addEventListener("click", function (evt) {
   }
 });
 
-
 childrenNumberLess.addEventListener("click", function (evt) {
   evt.preventDefault();
   --children.value;
@@ -73,5 +57,14 @@ childrenNumberMore.addEventListener("click", function (evt) {
   ++children.value;
   if (children.value > 99) {
     children.value = 99;
+  }
+});
+
+orderForm.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !adult.value || !children.value) {
+    evt.preventDefault();
+    orderForm.classList.remove("order-error");
+    orderForm.offsetWidth = orderForm.offsetWidth;
+    orderForm.classList.add("order-error");
   }
 });
